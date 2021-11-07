e79b5d50d82a0ddaeb84845fae6a424fa1130a6c
ST1LLY <st1lly@protonmail.com>
2021-11-07 15:56:06 +0300

    The path to the folder for logs changed

--- enviroment.py
+++ enviroment.py
@@ -5,7 +5,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # To initialize paths to directories
 TEMP_DIR = os.path.join(ROOT_DIR, 'temp')
-LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
+LOGS_DIR = os.path.join(ROOT_DIR, 'files', 'logs')
 
 # Path to dir with hashcat restore files
 HASHCAT_RESTORES_DIR = os.path.join(ROOT_DIR, 'files', 'restores')
