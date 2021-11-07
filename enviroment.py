78bc4a24e7efcbfb53c657e11bf498be44dca72f
ST1LLY <st1lly@protonmail.com>
2021-11-07 22:07:14 +0300

    unnecessary deleted

--- enviroment.py
+++ enviroment.py
@@ -4,7 +4,6 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # To initialize paths to directories
-TEMP_DIR = os.path.join(ROOT_DIR, 'temp')
 LOGS_DIR = os.path.join(ROOT_DIR, 'files', 'logs')
 
 # The path to the dir with hashcat restore files
