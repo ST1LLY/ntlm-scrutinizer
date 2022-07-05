2d7285b7bed28794ee80cf9351db22ea5e8f2e40
ST1LLY <st1lly@protonmail.com>
2022-07-05 17:17:48 +0400

    feat: add custom logger

--- app.py
+++ app.py
@@ -2,12 +2,17 @@
 Running FastAPI app
 """
 
+import os
 import logging
-
 from fastapi import FastAPI
+
+import modules.support_functions as sup_f
 from routers import instance, creds, technical
 
-logging.getLogger().setLevel(logging.DEBUG)
+sup_f.init_custome_logger(os.path.join('logs', 'api_all.log'), os.path.join('logs', 'api_error.log'))
+
+# https://stackoverflow.com/a/72238392/14642295
+logging.getLogger('uvicorn').removeHandler(logging.getLogger('uvicorn').handlers[0])
 
 app = FastAPI()
 app.include_router(instance.router)
