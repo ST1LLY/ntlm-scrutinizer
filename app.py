53fd299fb3fa85c49488f85f59c32ad141fbc0c4
ST1LLY <st1lly@protonmail.com>
2022-07-26 15:31:32 +0400

    fix: check uvicorn logger handlers

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ from routers import dump_ntlm, brute_ntlm, creds, technical
 
 sup_f.init_custome_logger(os.path.join(LOGS_DIR, 'api_all.log'), os.path.join(LOGS_DIR, 'api_error.log'))
 
-# https://stackoverflow.com/a/72238392/14642295
-logging.getLogger('uvicorn').removeHandler(logging.getLogger('uvicorn').handlers[0])
+
+if logging.getLogger('uvicorn').handlers:
+    # https://stackoverflow.com/a/72238392/14642295
+    logging.getLogger('uvicorn').removeHandler(logging.getLogger('uvicorn').handlers[0])
 
 app = FastAPI()
 app.include_router(dump_ntlm.router)
